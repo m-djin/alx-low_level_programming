02083a4d5bfebf78652a67b347de5954d1618341
m-djin <mnkdjin@st.ug.edu.gh>
2023-10-12 17:46:28 -0700

    fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,17 @@
 
 void print_diagonal(int n)
 {
-		int i, j;
+	int i, j;
 
-		if (n <= 0)
-                	_putchar('\n');
-		for (i = 0 ; i < n ; i++)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j < i ; j++)
 		{
-			for (j = 0 ; j <= i ; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
