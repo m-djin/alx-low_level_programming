f2caef3725a725b9ead324b743f31fa734efbe7d
m-djin <mnkdjin@st.ug.edu.gh>
2023-10-31 15:36:13 -0700

    advanced

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	char *output;
 
 	if (s1 == NULL)
-		s1 = NULL;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		s1len++;
 	for (i = 0 ; s2[i] != '\0' ; i++)
