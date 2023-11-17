c8c606086a8390b5ac8d187b11c1703e1e7f84a3
m-djin <mnkdjin@st.ug.edu.gh>
2023-11-16 18:28:15 -0800

    returns the number of elements in a linked list_t list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head of node
+ * Return: number of elements in a linked
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node++;
+	}
+
+	return (node);
+}
