beefeccc0377c0325c5922026910238ed38e5db9
m-djin <mnkdjin@st.ug.edu.gh>
2023-11-16 20:36:52 -0800

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,12 @@ size_t print_list(const list_t *h)
 {
 	size_t node;
 
-	node = 0;
-
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 
 		node++;
