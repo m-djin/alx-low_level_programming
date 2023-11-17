43e2bb72da3785c7402ad63423f84c2245ee0806
m-djin <mnkdjin@st.ug.edu.gh>
2023-11-16 20:53:56 -0800

    lets see

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node;
+	size_t node = 0;
 
 	while (h)
 	{
@@ -17,7 +17,6 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-
 		node++;
 	}
 	return (node);
