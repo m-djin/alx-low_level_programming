54d290b85a75d99d3c0248be1a20fa19179fc3cb
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-15 10:36:31 -0800

    create a dynamic library

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add two number
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - minus two number
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiply two number
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int mul(int a, int b)
+{       
+        return (a * b);
+}
+
+/**
+ * div - divide two number
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - modulus two number
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
