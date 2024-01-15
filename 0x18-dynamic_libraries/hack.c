1c3204a8970a7fa3c4ca0de95929781e8b6fe3bb
m-djin <mnkdjin@st.ug.edu.gh>
2024-01-15 12:06:45 -0800

    lets see

---
+++ 0x18-dynamic_libraries/hack.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 + 9\n", 17);
+	write(1, "Congratulations, you Win the Giga Millions!\n", 44);
+	exit(EXIT_SUCCESS);
+}
