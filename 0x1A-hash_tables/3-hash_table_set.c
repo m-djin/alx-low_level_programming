1f625834a92abe19701a2d31b12c3da5edcc6c90
m-djin <mnkdjin@st.ug.edu.gh>
2024-02-02 05:26:58 -0800

    Hash tables project 8 all tasks done 7 mandatory and 1 advanced

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: A pointer
+ * @key: the key
+ * @value: The value of the key
+ * Return: 0 or 1
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int ind, x;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	ind = key_index((const unsigned char *)key, ht->size);
+	for (x = ind; ht->array[x]; x++)
+	{
+		if (strcmp(ht->array[x]->key, key) == 0)
+		{
+			free(ht->array[x]->value);
+			ht->array[x]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[ind];
+	ht->array[ind] = new;
+
+	return (1);
+}
